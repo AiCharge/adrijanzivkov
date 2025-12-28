c46cf07fe9b9b1b2a588fe703ab0d0c21131d1ad
JordanchoEftimov <ejordancho@gmail.com>
2025-12-28 02:55:27 +0100

    password attr

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -47,11 +46,6 @@ class User extends Authenticatable implements FilamentUser
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function password(): Attribute
-    {
-        return Attribute::set(fn (string $val) => bcrypt($val));
     }
 
     public function canAccessPanel(Panel $panel): bool
